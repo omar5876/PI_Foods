f39c8c60f8431be5a8e6aed6c88a1387bdcc0095
unknown <omarramirez5876@gmail.com>
2022-07-16 12:28:42 -0500

    database done

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,25 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    healthScore: {
+      type: DataTypes.INTEGER
+    },
+    steps: {
+      type: DataTypes.ARRAY(DataTypes.STRING)
+    }
+  }, {timestamps: false});
 };
