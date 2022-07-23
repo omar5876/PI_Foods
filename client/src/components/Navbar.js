263771e83c4ff7d875830916d7cfd19df522c2e0
unknown <omarramirez5876@gmail.com>
2022-07-23 16:54:26 -0500

    navbar ended

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,9 @@ const Navbar = () => {
     return (
         <div className={s.navbarContainer}>
             <div className={s.navbarItems}>
-                <img src={navImage} alt="navbarImg"/> 
+                <Link to={'/Home'}>
+                <img className={s.navbarLogo} src={navImage} alt="navbarImg"/>
+                </Link>
 
             <ul >
                 <li><Link className={s.navbarLinks} to={'/Home'}>All Recipes</Link></li>
