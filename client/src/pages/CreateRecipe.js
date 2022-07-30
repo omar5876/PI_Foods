d22a90a81055e82c72f726b1b44c81601e16a72e
unknown <omarramirez5876@gmail.com>
2022-07-30 15:05:23 -0500

    making the form

--- client/src/pages/CreateRecipe.js
+++ client/src/pages/CreateRecipe.js
@@ -1,10 +1,17 @@
 import s from '../css/CreateRecipe.module.css'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import { getDiets } from '../redux/Actions'
 
 const CreateRecipe = () => {
     let [objectStep, setObjectStep] = useState({number: 0, step: ''})
     let [arraySteps, setArraySteps] = useState([])
-    let [input, setInput] = useState({name: '', summary: '', healthScore: 0, steps: arraySteps})
+    let [input, setInput] = useState({name: '', summary: '', image: '', healthScore: 0, steps: arraySteps, diets: []})
+
+    let diets = useSelector(state => state.getDiets)
+    console.log(diets)
+
+    let dispatch = useDispatch()
 
     const handleChange = (e) => {
         setInput({
@@ -26,15 +33,20 @@ const CreateRecipe = () => {
         console.log(arraySteps)
         setObjectStep({number: 0, step: ''})
     }
+
+    useEffect(() => {
+        dispatch(getDiets())
+    }, [dispatch])
     
     return (
         <div className={s.createRecipeContainer}>
-            Create Recipe
             <form>
                 <label>Name</label>
                 <input type={'text'} placeholder='write a name' required name='name' value={input.name} onChange={handleChange}/>
                 <label>Summary</label>
                 <textarea type={'text'} placeholder='write a name' required name='summary' value={input.summary} onChange={handleChange}/>
+                <label>Image</label>
+                <input type={'text'} placeholder='Put an url' required name='image' value={input.image} onChange={handleChange}/>
                 <label>Health Score</label>
                 <input type={'number'} placeholder='write a name' name='healthScore' value={input.healthScore} onChange={handleChange}/>
                 <label>Steps</label>
@@ -44,7 +56,34 @@ const CreateRecipe = () => {
                         <label>Step</label>
                         <textarea type={'text'}  name='step' value={objectStep.step} onChange={handleChangeObjectStep}/>
                         <button type='button' onClick={addStep}>Add Step</button>
+                        {!!input.steps.length &&
+                        <div>
+                            {input.steps.map((e, k) => {
+                                return (
+                                    <table key={k}>
+                                        <tr>
+                                            <th>Number</th>
+                                            <td>{e.number}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Step</th>
+                                            <td>{e.step}</td>
+                                        </tr>
+                                    </table>
+                                )
+                            })}    
+                        </div>}
                     </div>
+
+                {diets.length && (
+                    <>
+                    <label>Diets</label>
+                    <select>
+                        <option  disabled selected>Choose a Type of Diet</option>
+                        {diets.map((e, k) => <option key={k} value={e.name}>{e.name}</option>)}
+                    </select>
+                    </>
+                )}
                 <input type='submit' value='Create Recipe'/>
 
             </form>
