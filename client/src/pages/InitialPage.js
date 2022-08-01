2bb681517222236c6e84b6822374ec630fbce55d
unknown <omarramirez5876@gmail.com>
2022-08-01 17:25:25 -0500

    finishing the css

--- client/src/pages/InitialPage.js
+++ client/src/pages/InitialPage.js
@@ -1,10 +1,11 @@
+import s from '../css/InitialPage.module.css'
 import { Link } from "react-router-dom"
 
 const InitialPage = () => {
     return (
-        <div>
+        <div className={s.initialPageContainer}>
             <h2>Learn about cooking</h2>
-            <Link to={'/Home'}>
+            <Link className={s.initialPageButton} to={'/Home'}>
                 <button>Enter</button>
             </Link>
         </div>
